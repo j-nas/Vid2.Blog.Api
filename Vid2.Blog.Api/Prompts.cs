1f9fab10e535177bd822be359d4c1da563e257f7
Jon Naske <jon@shorecel.com>
2023-11-27 17:43:03 -0800

    provide maximum token lengths with gpt-3.5-turbo

--- Vid2.Blog.Api/Prompts.cs
+++ Vid2.Blog.Api/Prompts.cs
@@ -2,9 +2,10 @@ namespace Vid2.Blog.Api;
 
 public static class Prompts
 {
-    public static string CreateCondensePrompt(int percentage) =>
-        $"You will condense this video transcript to about {percentage}% of its original length. You keep every detail, even if you think it doesn't matter. You keep the tone and style of speaking as well";
+    public static string CreateCondensePrompt(string transcript,
+        int percentage = 50) =>
+        $"You will condense this video transcript to about {percentage}% of its original length. You keep every detail, even if you think it doesn't matter. You keep the tone and style of speaking as well. The transcript follows: {transcript}";
 
-    public static string CreateBlogPostPrompt() =>
-        "You will create a blog post from this transcript. You will write it from an expert's point of view, as if you were the one who made the video. You will format the output as markdown. Please make it at least 1500 words long.";
+    public static string CreateBlogPostPrompt(string transcript) =>
+        $"You will create a blog post from this condensed transcript. You will write it from an expert's point of view, as if you were the one who made the video. You will format the output as markdown. Please make it at least 1500 words long. The transcript follows {transcript}";
 }
--- Vid2.Blog.Api/Services/TranscriptionService.cs
+++ Vid2.Blog.Api/Services/TranscriptionService.cs
@@ -1,13 +1,16 @@
 using OpenAI.Interfaces;
 using OpenAI.ObjectModels.RequestModels;
+using OpenAI.Tokenizer.GPT3;
 
 namespace Vid2.Blog.Api.Services
 {
     public class TranscriptionService(IOpenAIService openAiService, IYtdlService ytdlService) : ITranscriptionService
     {
+        private readonly int _maxTokens = 4096;
         public async Task<string> GenerateBlogPost(string videoId)
         {
               var transcript = await ytdlService.GetTranscript(videoId);
+              // var condensedTranscript = GetCondensedTranscript(transcript);
             var blogpostResult =
                 await openAiService.ChatCompletion.CreateCompletion(
                     new ChatCompletionCreateRequest
@@ -15,12 +18,15 @@ namespace Vid2.Blog.Api.Services
                         Messages = new List<ChatMessage>
                         {
                             ChatMessage.FromSystem(
-                                Prompts.CreateBlogPostPrompt()),
+                                "You are a very helpful assistant, who is an expert in creating blog posts from video transcript"),
                             ChatMessage.FromUser(
-                                $"Please make a blog from the following: {transcript}")
+                                Prompts.CreateBlogPostPrompt(transcript))
 
                         },
-                        Model = OpenAI.ObjectModels.Models.Gpt_4
+                        
+                        Model = OpenAI.ObjectModels.Models.Gpt_3_5_Turbo_16k,
+                        MaxTokens = _maxTokens
+                        
                     });
             if (!blogpostResult.Successful)
             {
@@ -29,5 +35,28 @@ namespace Vid2.Blog.Api.Services
 
             return blogpostResult.Choices.First().Message.Content;
         }
+
+        // private async Task<string> GetCondensedTranscript(string transcript)
+        // {
+        //     var condensedTranscript =
+        //         await openAiService.ChatCompletion.CreateCompletion(
+        //             new ChatCompletionCreateRequest
+        //             {
+        //                 Messages = new List<ChatMessage>
+        //                 {
+        //                     ChatMessage.FromSystem(
+        //                         "You are a helpful assistant, who specialized in condensing video transcripts."),
+        //                     ChatMessage.FromUser(Prompts.CreateCondensePrompt(transcript))
+        //                 },
+        //                 Model = OpenAI.ObjectModels.Models.Gpt_3_5_Turbo,
+        //                 MaxTokens = _maxTokens
+        //             });
+        // }
+        //
+        // private int GetAmountToCondense(string transcript)
+        // {
+        //     var transcriptTokens = TokenizerGpt3.TokenCount(transcript);
+        //     return transcriptTokens.Length;
+        // }
     }
 }
